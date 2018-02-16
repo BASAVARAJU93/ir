ae96d2e4ac7afe55e4bd7f2fc6cdcaba0e71a6fa
BASAVARAJU93 <36529954+BASAVARAJU93@users.noreply.github.com>
2018-02-16 17:22:49 +0530

    Update xy.py

--- xy.py
+++ xy.py
@@ -1,4 +1,4 @@
-a=int(input("Enter an integer num:")
-b=int(input("Enter an integer num:")
+a=int(input("Enter a value of a:"))
+b=int(input("Enter a value of b:"))
 result=(a*a)+(b*b)+(2*a*b)
-print("The result is:",result)
+print("The result is:",result "!!")
